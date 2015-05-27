f760b23e36f421ea42d96cd9498d4d46f28b4f22
Tino Steinort <tinosteinort@live.de>
2015-05-27 18:30:44 +0200

    Add ServiceLoader Mechanism for Loading available MarshallingConfigProviders

--- simple-rest-facade/src/main/java/de/tse/simplerestfacade/marshalling/MarshallingConfigProvider.java
+++ simple-rest-facade/src/main/java/de/tse/simplerestfacade/marshalling/MarshallingConfigProvider.java
@@ -1,5 +1,36 @@
 package de.tse.simplerestfacade.marshalling;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.ServiceLoader;
+
+
 public class MarshallingConfigProvider {
 
+    private final List<MarshallingConfig> configs = new ArrayList<>();
+    
+    public MarshallingConfigProvider() {
+        loadMarshallingProvider();
+    }
+
+    private void loadMarshallingProvider() {
+        for (MarshallingConfig marshallingConfig : ServiceLoader.load(MarshallingConfig.class)) {
+            configs.add(marshallingConfig);
+        }
+    }
+    
+    public Marshaller getMarshaller(final String mediaType) {
+        final Optional<MarshallingConfig> configOption = configs.stream()
+                .filter(config -> config.supportsMediaType(mediaType)).findFirst();
+
+        return configOption.orElseThrow(() -> new IllegalStateException()).getMarshaller();
+    }
+    
+    public Unmarshaller getUnmarshaller(final String mediaType) {
+        final Optional<MarshallingConfig> configOption = configs.stream()
+                .filter(config -> config.supportsMediaType(mediaType)).findFirst();
+
+        return configOption.orElseThrow(() -> new IllegalStateException()).getUnmarshaller();
+    }
 }
