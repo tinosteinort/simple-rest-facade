f47b4ed3166a0e867aa996717c111a2663299000
Tino Steinort <tinosteinort@live.de>
2015-07-27 21:16:45 +0200

    Add Possibility to configure additional MarshallingConfigs

--- simple-rest-facade-core/src/main/java/de/tse/simplerestfacade/DefaultRestFacadeFactory.java
+++ simple-rest-facade-core/src/main/java/de/tse/simplerestfacade/DefaultRestFacadeFactory.java
@@ -3,11 +3,14 @@ package de.tse.simplerestfacade;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.apache.http.client.HttpClient;
 
 import de.tse.simplerestfacade.invocation.RestInvocationHandler;
+import de.tse.simplerestfacade.marshalling.MarshallingConfig;
 import de.tse.simplerestfacade.marshalling.MarshallingConfigProvider;
 
 public class DefaultRestFacadeFactory implements RestFacadeFactory {
@@ -19,18 +22,23 @@ public class DefaultRestFacadeFactory implements RestFacadeFactory {
 	private final String defaultMediaType;
 	private final Optional<RestInterfaceValidator> validator;
 	private final ExceptionHandler defaultExceptionHandler;
-    private final MarshallingConfigProvider marshallingConfigProvider = new MarshallingConfigProvider();
+    private final MarshallingConfigProvider marshallingConfigProvider;
 	
-	public DefaultRestFacadeFactory(final URI endpoint, final HttpClient httpClient, final String defaultMediaType, final boolean validateRest, final ExceptionHandler exceptionHandler) {
+    public DefaultRestFacadeFactory(final URI endpoint, final HttpClient httpClient, final String defaultMediaType,
+            final boolean validateRest, final ExceptionHandler exceptionHandler,
+            final List<MarshallingConfig> additionalMarshallingConfigs) {
+        
 	    this.endpoint = endpoint;
 		this.httpClient = httpClient;
 		this.defaultMediaType = defaultMediaType;
 		this.validator = validateRest ? Optional.of(new DefaultRestInterfaceValidator()) : Optional.empty();
 		this.defaultExceptionHandler = exceptionHandler;
+		this.marshallingConfigProvider = new MarshallingConfigProvider(additionalMarshallingConfigs);
 	}
 	
     public DefaultRestFacadeFactory(final URI endpoint, final HttpClient httpClient, final String defaultMediaType) {
-        this(endpoint, httpClient, defaultMediaType, DEFAULT_VALIDATE_REST, new DefaultRestExceptionHandler());
+        this(endpoint, httpClient, defaultMediaType, DEFAULT_VALIDATE_REST, new DefaultRestExceptionHandler(),
+                Collections.emptyList());
     }
 
     @Override
@@ -43,7 +51,8 @@ public class DefaultRestFacadeFactory implements RestFacadeFactory {
 
 	    validator.ifPresent(validator -> validator.validate(facadeClass, mediaType));
 		
-	    final ExecutionContext executionContext = new ExecutionContext(endpoint, mediaType, httpClient, marshallingConfigProvider, exceptionHandler);
+        final ExecutionContext executionContext = new ExecutionContext(endpoint, mediaType, httpClient,
+                marshallingConfigProvider, exceptionHandler);
 		final InvocationHandler invocationHandler = new RestInvocationHandler(executionContext);
 		
 		return createProxy(facadeClass, invocationHandler);
@@ -51,6 +60,7 @@ public class DefaultRestFacadeFactory implements RestFacadeFactory {
 	
 	@SuppressWarnings("unchecked")
     protected <T> T createProxy(final Class<T> facadeClass, final InvocationHandler invocationHandler) {
-        return (T) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { facadeClass }, invocationHandler);
+        return (T) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { facadeClass },
+                invocationHandler);
     }
 }
--- simple-rest-facade-core/src/main/java/de/tse/simplerestfacade/marshalling/MarshallingConfigProvider.java
+++ simple-rest-facade-core/src/main/java/de/tse/simplerestfacade/marshalling/MarshallingConfigProvider.java
@@ -5,12 +5,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
+import de.tse.simplerestfacade.RestClientException;
+
 
 public class MarshallingConfigProvider {
 
     private final List<MarshallingConfig> configs = new ArrayList<>();
     
-    public MarshallingConfigProvider() {
+    public MarshallingConfigProvider(final List<MarshallingConfig> additionalConfigs) {
+        if (additionalConfigs != null) {
+            configs.addAll(additionalConfigs);
+        }
         loadMarshallingProvider();
     }
 
@@ -24,13 +29,13 @@ public class MarshallingConfigProvider {
         final Optional<MarshallingConfig> configOption = configs.stream()
                 .filter(config -> config.supportsMediaType(mediaType)).findFirst();
 
-        return configOption.orElseThrow(() -> new IllegalStateException()).getMarshaller();
+        return configOption.orElseThrow(() -> new RestClientException("No Marshaller found")).getMarshaller();
     }
     
     public Unmarshaller getUnmarshaller(final String mediaType) {
         final Optional<MarshallingConfig> configOption = configs.stream()
                 .filter(config -> config.supportsMediaType(mediaType)).findFirst();
 
-        return configOption.orElseThrow(() -> new IllegalStateException()).getUnmarshaller();
+        return configOption.orElseThrow(() -> new RestClientException("No Unmarshaller found")).getUnmarshaller();
     }
 }
